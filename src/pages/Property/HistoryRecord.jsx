208281142024b1ddbf574e1455e5cff13a636ce7
Liang081821 <2001henry99@gmail.com>
2024-09-17 13:51:26 +0800

    docs: adjust files structure

--- src/pages/Property/HistoryRecord.jsx
+++ src/pages/Property/HistoryRecord.jsx
@@ -1,28 +1,45 @@
+import Saving from "./Saving";
+import Consume from "./Consume";
+import Invest from "./Invest";
+import AddNewFunction from "./addNewAccount";
+
 export default function HistoryRecord() {
   return (
-    <div className="m-14 flex h-[100px] items-center gap-4 border border-black">
-      <div>11/24</div>
-      <div className="flex w-full">
-        <div className="h-2 w-14 bg-[#222E50]"></div>
-        <div className="h-2 w-full bg-[#9DBEBB]"></div>
-        <div className="h-2 w-14 bg-[#E6A602]"></div>
+    <>
+      <div className="flex w-full flex-col">
+        <div className="flex w-full justify-end">
+          <AddNewFunction></AddNewFunction>
+        </div>
+        <div className="flex flex-wrap items-center justify-center">
+          <Saving />
+          <Consume />
+          <Invest />
+        </div>
+        <div className="m-14 flex h-[100px] items-center gap-4 border border-black">
+          <div>11/24</div>
+          <div className="flex w-full">
+            <div className="h-2 w-14 bg-[#222E50]"></div>
+            <div className="h-2 w-full bg-[#9DBEBB]"></div>
+            <div className="h-2 w-14 bg-[#E6A602]"></div>
+          </div>
+          <div className="flex h-[60px] w-[110px] flex-col items-center justify-center border">
+            <div>儲蓄</div>
+            <div>55,785</div>
+          </div>
+          <div className="flex h-[60px] w-[110px] flex-col items-center justify-center border">
+            <div>消費</div>
+            <div>55,785</div>
+          </div>
+          <div className="flex h-[60px] w-[110px] flex-col items-center justify-center border">
+            <div>投資</div>
+            <div>55,785</div>
+          </div>
+          <div className="flex h-[60px] w-[110px] flex-col items-center justify-center border">
+            <div>總資產</div>
+            <div>55,785</div>
+          </div>
+        </div>
       </div>
-      <div className="flex h-[60px] w-[110px] flex-col items-center justify-center border">
-        <div>儲蓄</div>
-        <div>55,785</div>
-      </div>
-      <div className="flex h-[60px] w-[110px] flex-col items-center justify-center border">
-        <div>消費</div>
-        <div>55,785</div>
-      </div>
-      <div className="flex h-[60px] w-[110px] flex-col items-center justify-center border">
-        <div>投資</div>
-        <div>55,785</div>
-      </div>
-      <div className="flex h-[60px] w-[110px] flex-col items-center justify-center border">
-        <div>總資產</div>
-        <div>55,785</div>
-      </div>
-    </div>
+    </>
   );
 }
--- src/pages/Property/index.jsx
+++ src/pages/Property/index.jsx
@@ -1,23 +1,13 @@
-import Saving from "./Saving";
-import Consume from "./Consume";
-import Invest from "./Invest";
+// import Saving from "./Saving";
+// import Consume from "./Consume";
+// import Invest from "./Invest";
 import HistoryRecord from "./HistoryRecord";
-import AddNewFunction from "./addNewAccount";
+// import AddNewFunction from "./addNewAccount";
 
 export default function Property() {
   return (
     <>
-      <div className="flex w-full flex-col">
-        <div className="flex w-full justify-end">
-          <AddNewFunction></AddNewFunction>
-        </div>
-        <div className="flex flex-wrap items-center justify-center">
-          <Saving />
-          <Consume />
-          <Invest />
-        </div>
-        <HistoryRecord />
-      </div>
+      <HistoryRecord />
     </>
   );
 }
